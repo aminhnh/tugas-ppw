0d25ce72cbd3719cf8ada4c717d6cc81b8200bf0
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-18 19:38:03 +0700

    Add soal no 1 dan 2

--- pertemuan8/tugas/script.js
+++ pertemuan8/tugas/script.js
@@ -2,9 +2,17 @@ function takeAction() {
   alert("I am taking action!");
 }
 
-var kickstart = document.getElementById("kickstart-text");
 function editAlign() {
-  document.getElementById("align-text").innerHTML =
-    document.getElementById("kickstart-text").innerText;
-  console.log(document.getElementById("kickstart-text").innerText);
+  var kickstart = document.getElementById("kickstart-text").innerText;
+  var textarea = `<form><textarea name="" id="" cols="20" rows="5">${kickstart}</textarea><button type="submit" onclick="saveEdit()">Save Edit</button>
+  </form>`;
+  console.log(document.getElementById("align-text").innerHTML);
+  document.getElementById("align-text").innerHTML = textarea;
+  console.log(document.getElementById("align-text").innerHTML);
+}
+
+function saveEdit() {
+  console.log(document.getElementById("align-text").innerHTML);
+  // var currentText = document.getElementById("edited-text").value;
+  // document.getElementById("align-text").innerHTML = `<p>${currentText}</p>`;
 }
