78201cb58a0ca6a8c0487d763c08ac5508fe8244
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-18 20:35:12 +0700

    Improve number 2

--- pertemuan8/tugas/script.js
+++ pertemuan8/tugas/script.js
@@ -1,18 +1,18 @@
+var para = document.getElementById("align-para");
+var para2 = document.getElementById("kickstart-para");
+
 function takeAction() {
   alert("I am taking action!");
 }
 
-function editAlign() {
-  var kickstart = document.getElementById("kickstart-text").innerText;
-  var textarea = `<form><textarea name="" id="" cols="20" rows="5">${kickstart}</textarea><button type="submit" onclick="saveEdit()">Save Edit</button>
+function edit() {
+  para.innerHTML =
+    `<form><textarea name="" id="edited-text" cols="20" rows="4">` +
+    para2.innerText +
+    `</textarea><button type="submit" class="btn-edit" onclick="saveEdit()">Save Edit</button>
   </form>`;
-  console.log(document.getElementById("align-text").innerHTML);
-  document.getElementById("align-text").innerHTML = textarea;
-  console.log(document.getElementById("align-text").innerHTML);
 }
 
 function saveEdit() {
-  console.log(document.getElementById("align-text").innerHTML);
-  // var currentText = document.getElementById("edited-text").value;
-  // document.getElementById("align-text").innerHTML = `<p>${currentText}</p>`;
+  para.textContent = document.getElementById("edited-text").value;
 }
